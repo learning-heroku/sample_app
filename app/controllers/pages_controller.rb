c0bce835583691ab6cd259bbbdca426bc2337994
Learning Heroku <learning.heroku@gmail.com>
2011-09-11 20:38:39 +0100

    Sample app with CSS, partials and integration tests

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,17 @@
 class PagesController < ApplicationController
   def home
+	@title = "Home"
   end
 
   def contact
+	@title = "Contact"
   end
 
   def about
-	@title = "Ruby on Rails | About"
+	@title = "About"
   end
-
+  
+  def help
+    @title = "Help"
+  end
 end
